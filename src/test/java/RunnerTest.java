535a7733f00f72cabcaee46a7a21369f331ad57c
nagabalap7 <nagabalap7@gmail.com>
2016-08-16 22:41:43 +0530

    Create RunnerTest.java

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,16 @@
+package com.TickerMiser;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = "src/test/resources",
+		format = {"pretty","html:target/html/"}
+		)
+public class RunnerTest {
+
+}
